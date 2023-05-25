347ab01e518235d5cdf45d1792478c0b5c2636da
Cauane Cardoso <cauhcdz@gmail.com>
2023-05-25 13:42:57 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Atividade realizada por: Cauane Cardoso da Rosa
+# Curso: Análise e Desenvolvimento de Sistemas
+
+# Desenvolvimento da Atividade Somativa 1 referente a Semana 4.
+
+
+# Proposta da Atividade:
+
+•	Desenvolva as funcionalidades de incluir e listar estudantes
+•	Apenas o nome do estudante deve ser perguntado ao usuário (o restante das informações será inserido na próxima etapa da atividade).
+•	Os nomes dos estudantes devem ser armazenados em uma lista.
+•	Para a funcionalidade de listar, deve ser utilizada uma estrutura de repetição para percorrer a lista e mostrar os nomes cadastrados – vide Figura 1.
+•	Caso a lista de estudantes esteja vazia quando a opção de listar for acionada, deve-se mostrar uma mensagem “Não há estudantes cadastrados”.
+•	Caso o usuário selecione as opções professores, disciplinas, turmas ou matrículas no menu principal, o sistema deve mostrar a mensagem “EM DESENVOLVIMENTO”, e mostrar novamente o menu principal.
+•	Caso o usuário selecione as opções atualizar ou excluir no menu de operações do estudante, o sistema deve mostrar a mensagem “EM DESENVOLVIMENTO”, e mostrar novamente o menu de operações.
---
+++ main.py
@@ -0,0 +1,188 @@
+# Atividade realizada por: Cauane Cardoso da Rosa
+# Curso: Análise e Desenvolvimento de Sistemas
+
+
+estudantes = [] # lista
+
+def gerenciar_estudantes():
+    print("Opção selecionada: Gerenciar estudantes")
+  
+
+def gerenciar_professores():
+    print("Opção selecionada: Gerenciar professores")
+    
+
+def gerenciar_disciplinas():
+    print("Opção selecionada: Gerenciar disciplinas")
+  
+
+def gerenciar_turmas():
+    print("Opção selecionada: Gerenciar turmas")
+
+
+def gerenciar_matriculas():
+    print("Opção selecionada: Gerenciar matrículas")
+
+
+def menu_principal():
+    while True:
+        print("\nMenu Principal:")
+        print("1. Gerenciar estudantes")
+        print("2. Gerenciar professores")
+        print("3. Gerenciar disciplinas")
+        print("4. Gerenciar turmas")
+        print("5. Gerenciar matrículas")
+        print("6. Sair")
+
+        opcao = input("Selecione uma opção: ")
+
+        if opcao == "1":
+            if menu_gerenciar_estudantes():
+                print("Encerrando aplicação")
+                break
+        elif opcao == "2":
+            if menu_gerenciar_professores():
+                print("Encerrando aplicação")
+                break
+        elif opcao == "3":
+            if menu_gerenciar_disciplinas():
+                print("Encerrando aplicação")
+                break
+        elif opcao == "4":
+            if menu_gerenciar_turmas():
+                print("Encerrando aplicação")
+                break
+        elif opcao == "5":
+            if menu_gerenciar_matriculas():
+                print("Encerrando aplicação")
+                break
+        elif opcao == "6":
+            print("Encerrando aplicação")
+            break
+        else:
+            print("Opção inválida. Por favor, selecione uma opção válida.")
+
+def menu_gerenciar_professores():
+    while True:
+        print("\nMenu de Operações - Gerenciar Professores:")
+        print("1. Incluir")
+        print("2. Listar")
+        print("3. Atualizar")
+        print("4. Excluir")
+        print("5. Voltar ao menu principal")
+
+        opcao = input("Selecione uma opção: ")
+
+        if opcao == "1" or opcao == "2" or opcao == "3" or opcao == "4":
+            print("\n=== EM DESENVOLVIMENTO ===")
+            return menu_principal() 
+        elif opcao == "5":
+            return False  
+        else:
+            print("Opção inválida. Por favor, selecione uma opção válida.")
+    
+            
+def menu_gerenciar_estudantes():
+    while True:
+        print("\nMenu de Operações - Gerenciar Estudantes:")
+        print("1. Incluir")
+        print("2. Listar")
+        print("3. Atualizar (EM DESENVOLVIMENTO)")
+        print("4. Excluir (EM DESENVOLVIMENTO)")
+        print("5. Voltar ao menu principal")
+
+        opcao = input("Selecione uma opção: ")
+
+        if opcao == "1":
+            incluir_estudante()
+        elif opcao == "2":
+            listar_estudantes()
+        elif opcao == "3" or opcao == "4":
+            print("\n=== EM DESENVOLVIMENTO ===")
+            return menu_principal()
+        elif opcao == "5":
+            return False
+        else:
+            print("Opção inválida. Por favor, selecione uma opção válida.")
+
+        return True
+
+
+def incluir_estudante():
+    while True:
+        nome = input("Digite o nome do estudante ('v' para voltar): ")
+        if nome == 'v':
+            return menu_gerenciar_estudantes()
+        estudantes.append(nome)
+        print("\nEstudante adicionado com sucesso!")
+
+
+def listar_estudantes():
+    if len(estudantes) == 0:
+        print("Não há estudantes cadastrados.")
+    else:
+        print("\nEstudantes cadastrados:")
+        for estudante in estudantes:
+            print("- {}".format(estudante))
+        input("Aperte Enter para voltar")
+    return menu_gerenciar_estudantes()
+            
+
+def menu_gerenciar_disciplinas():
+    while True:
+        print("\nMenu de Operações - Gerenciar Disciplinas:")
+        print("1. Incluir")
+        print("2. Listar")
+        print("3. Atualizar")
+        print("4. Excluir")
+        print("5. Voltar ao menu principal")
+
+        opcao = input("Selecione uma opção: ")
+
+        if opcao == "1" or opcao == "2" or opcao == "3" or opcao == "4":
+            print("\n=== EM DESENVOLVIMENTO ===")
+            return menu_principal() 
+        elif opcao == "5":
+            return False  
+        else:
+            print("Opção inválida. Por favor, selecione uma opção válida.")
+            
+def menu_gerenciar_turmas():
+    while True:
+        print("\nMenu de Operações - Gerenciar Turmas:")
+        print("1. Incluir")
+        print("2. Listar")
+        print("3. Atualizar")
+        print("4. Excluir")
+        print("5. Voltar ao menu principal")
+
+        opcao = input("Selecione uma opção: ")
+
+        if opcao == "1" or opcao == "2" or opcao == "3" or opcao == "4":
+            print("\n=== EM DESENVOLVIMENTO ===")
+            return menu_principal()  
+        elif opcao == "5":
+            return False  
+        else:
+            print("Opção inválida. Por favor, selecione uma opção válida.")
+            
+def menu_gerenciar_matriculas():
+    while True:
+        print("\nMenu de Operações - Gerenciar Matriculas:")
+        print("1. Incluir")
+        print("2. Listar")
+        print("3. Atualizar")
+        print("4. Excluir")
+        print("5. Voltar ao menu principal")
+
+        opcao = input("Selecione uma opção: ")
+
+        if opcao == "1" or opcao == "2" or opcao == "3" or opcao == "4":
+            print("\n=== EM DESENVOLVIMENTO ===")
+            return menu_principal()  
+        elif opcao == "5":
+            return False  
+        else:
+            print("Opção inválida. Por favor, selecione uma opção válida.")
+
+menu_principal()
